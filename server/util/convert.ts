913f64f154cc776c5978fb2ef6eca3f31ab0ef2c
DoungChan <doungchansroeun@gmail.com>
2025-12-07 15:59:35 +0700

    feat: enhance HTML output with improved styling and structure for DOCX conversion

--- server/util/convert.ts
+++ server/util/convert.ts
@@ -19,11 +19,79 @@ export async function convertDocxToHtml(inputPath: string, outputPath: string) {
 
   const html = `<!doctype html>
 <html lang="en">
-<head>
-  <meta charset="utf-8"/>
-  <meta name="viewport" content="width=device-width,initial-scale=1"/>
-  <title>Document</title>
-</head>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="./activate_app_conditions_en_v3_files/css2" rel="stylesheet" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nokora:wght@100..900&family=Victor+Mono:ital,wght@0,100..700;1,100..700&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <style>
+      body {
+        background-color: transparent;
+        color: #000;
+        padding: 0;
+        margin: 0px 30px 50px;
+      }
+
+      body,
+      h1 {
+        font-size: 12px;
+                font-family: "Work Sans", "Nokora";
+        line-height: 22px;
+      }
+
+      ol {
+        padding-left: 30px;
+      }
+
+      ol li {
+        padding-left: 6px;
+      }
+
+      .wrap-order-number {
+        margin-left: 36px;
+      }
+
+      .wrap-order-number,
+      .wrap-order-number h1,
+      .wrap-order-number p {
+        position: relative;
+        word-break: break-word;
+      }
+
+      .order-number {
+        position: absolute;
+        left: -36px;
+        top: 0px;
+      }
+
+      a {
+        color: #fff;
+      }
+
+      table td {
+        padding: 10px !important;
+      }
+
+      .order-list-dashed {
+        padding: 0px
+      }
+
+      .order-list-dashed li {
+        padding-left: 15px;
+        position: relative;
+      }
+
+      .order-list-dashed li:before {
+        content: "-";
+        position: absolute;
+        left: 0px;
+      }
+
+    </style>
+  </head>
 <body>
 ${result.value}
 </body>
