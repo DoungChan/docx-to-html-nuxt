15749317db35174ff21b2bd21283e784f13ac2a8
DoungChan <doungchansroeun@gmail.com>
2025-12-07 16:59:18 +0700

    feat: simplify app layout by removing loading state and unnecessary conditional rendering

--- app/app.vue
+++ app/app.vue
@@ -1,12 +1,10 @@
 <template>
-  <div v-if="!pending">
+  <div>
     <NuxtRouteAnnouncer />
     <NuxtPage />
   </div>
-  <div v-else class="m-auto bg-gray-50">
-    <p class="text-gray-500 text-lg">Loading...</p>
-  </div>
+
 </template>
 <script setup>
-const { pending } = useAsyncData('app-init', () => new Promise(resolve => setTimeout(resolve, 100)));
+
 </script>
